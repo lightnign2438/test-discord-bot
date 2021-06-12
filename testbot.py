f5b4880063a859eef0da736e8c14af5c17dbdb81
Lightning2438 <littlehutch23@hotmail.com>
2021-06-12 11:03:56 -0500

    added some changes

--- testbot.py
+++ testbot.py
@@ -10,12 +10,14 @@ async def on_ready():
     print("Run successful")
 @testbot.event
 async def on_message(message):
+    print(message.content)
     if message.content.lower() == "$hello":
         await message.channel.send("Hi!")
-    if "$echo" in message.content.lower():
+    if "$echo " in message.content.lower():
+        print("test") 
         await message.channel.send(message.content[6::])
     #fix 
-    if "$color" in message.content.lower():
+    if  "$color" in message.content.lower():
         desiredcolor = message.content[7::]
         ahex = matplotlib.colors.to_rgb('#'+desiredcolor)
         print(ahex)
@@ -26,13 +28,13 @@ async def on_message(message):
         print(cac)
         print(caa)
         await message.author.top_role.edit(color = discord.Color.from_rgb(int(cad),int(cac),int(caa)))
-    if "$randcolor" in message.content.lower():
+    if  "$randcolor" in message.content.lower():
         await message.author.top_role.edit(color = discord.Color.random())
         await message.channel.send("Color changed")
-    if "$name" in message.content.lower():
+    if  "$name" in message.content.lower():
         newname = message.content[6::]
         await message.author.top_role.edit(name = newname)
-    if "$perm" in message.content.lower():
+    if  "$perm" in message.content.lower():
         perm = message.content[6::]        
         if perm == "nickname":    
             await message.channel.send(message.author.top_role.permissions.manage_nicknames)
@@ -40,7 +42,7 @@ async def on_message(message):
             await message.channel.send(message.author.top_role.permissions.manage_roles)
         else:
             await message.channel.send("Something went wrong, please try again")
-    if "$nick" in message.content.lower():
+    if "$nick"in message.content.lower():
             newnick = message.content[6::]
             await message.author.edit(nick = newnick)
     if "$memberlist" in message.content.lower():
@@ -50,17 +52,63 @@ async def on_message(message):
         selecteduser = memlist[random.randint(0, len(memlist)-1)]
         await message.channel.send(selecteduser.mention)
     if "$channel" in message.content.lower():
-        print(message.guild.channels)
+        for i in message.guild.channels:
+            print(i)
+        
     if "$join" in message.content.lower():
         VC = message.guild.voice_channels[0]
         await message.guild.change_voice_state(channel = VC)
     if "$ban" in message.content.lower():
         banuser = message.content[5::]
-        user = int(banuser)
+        user = str(banuser)
         await discord.Guild.ban('code.test', reason = "somebody banned you", delete_message_days=7)            
-
-
-
+    if  "$clone" in message.content.lower():
+        currchan = message.channel
+        name = message.content[8::]
+        await currchan.clone(name=name)
+    if  message.content.lower() == "$delete channel":
+        await message.channel.delete()
+    if  "$deltchan" in message.content.lower():
+        channame=message.content[10::]
+        for i in message.guild.text_channels:
+            if channame==i:
+                deltachan=i
+                break
+        else:
+            await message.channel.send("Channel not found, please try again")
+        await deltachan.delete()
+    if "$createtchan" in message.content.lower():
+        channame = message.content[12::]
+        await message.guild.create_text_channel(channame)
+    if "$mymes" in message.content.lower():
+        counter=0
+        async for i in message.channel.history():
+            counter+=1
+        await message.channel.send("There are " + str(counter) + " messages in this channel by " + str(message.author))
+    if "$mymes" in message.content.lower():
+        counter=0
+        async for i in message.channel.history():
+            if message.author==i.author:
+                counter+=1
+        await message.channel.send("There are " + str(counter) + " messages in this channel by " + str(message.author))
+    if message.content.lower() == "$invite":
+        invitee = await message.channel.create_invite(max_age = 120,max_uses = 1, unique =True)
+        await message.channel.send(str(invitee))
+    if message.content.lower()[0:7] == "$chodm ":
+        param = message.content[7::]
+        if message.author.dm_channel == None:
+            dmchan = await message.author.create_dm()
+        else:
+            dmchan = message.author.dm_channel
+        await dmchan.send(param)
+    if message.content.lower() == "$dminv ":
+        user = message.content.author()
+        invitee = await message.channel.create_invite(max_age = 120,max_uses = 1, unique =True)
+        if message.author.dm_channel == None:
+            dmchan = await message.author.create_dm()
+        else:
+            dmchan = message.author.dm_channel
+        await dmchan.send(invitee)
 
 testbot.run(os.environ['token'])
 
@@ -70,5 +118,6 @@ errors:
 invalid literal for int() with base 16
 sol:
 input isn't str, it's an object.
+bug: echo doesn't work
 '''
 
